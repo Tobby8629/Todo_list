b6531c8173d0d1684b3b2dc7409bded1a905f721
tobby8629 <sarhmullah15@gmail.com>
2023-01-22 15:10:05 -0800

    update todo

--- src/module/form.js
+++ src/module/form.js
@@ -4,37 +4,36 @@ import more from './more.js';
 const form = () => {
   const master = document.querySelector('#master');
   const fort = document.querySelector('#form');
-  const get = document.querySelector('#name');
+  const geh = document.querySelector('#name');
 
   const todo = new Arr();
 
   fort.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (get.value === '') {
+    if (geh.value === '') {
       return;
     }
     master.innerHTML = '';
-    todo.addTodo(get.value, master);
-    get.value = '';
+    todo.addTodo(geh.value, master);
+    geh.value = '';
     localStorage.setItem('store', JSON.stringify(todo.todos));
-
-    const gro = document.querySelectorAll('#box');
-    const check = document.querySelectorAll('#check');
-
-    todo.onfresh();
 
     more(todo);
 
-    todo.changecomplete(check, gro);
-
     const remove = document.querySelectorAll('#trash');
     remove.forEach((re, index) => re.addEventListener('click', () => {
-      const very = index + 1;
+      const very = index;
       re.parentElement.parentElement.remove();
       todo.removeTodo(very);
       todo.changeindex();
       localStorage.setItem('store', JSON.stringify(todo.todos));
     }));
+
+    
+    const get = document.querySelectorAll('#box');
+    const check = document.querySelectorAll('#check');
+    todo.changecomplete(check, get);
+    todo.onfresh();
 
     const clean = document.querySelector('#clear');
 
@@ -49,22 +48,24 @@ const form = () => {
     const replay = JSON.parse(localStorage.getItem('store'));
     todo.reload(replay, master);
 
-    const gro = document.querySelectorAll('#box');
-    const check = document.querySelectorAll('#check');
-
     more(todo);
-    todo.changecomplete(check, gro);
-    todo.linetr();
-
+  
     const remove = document.querySelectorAll('#trash');
     remove.forEach((re, index) => re.addEventListener('click', () => {
-      const very = index + 1;
+      const very = index
       re.parentElement.parentElement.remove();
       todo.removeTodo(very);
       todo.changeindex();
       localStorage.setItem('store', JSON.stringify(todo.todos));
     }));
   });
+
+  const get = document.querySelectorAll('#box');
+  const check = document.querySelectorAll('#check');
+  todo.changecomplete(check, get);
+
+  
+  todo.linetr();
 
   const clean = document.querySelector('#clear');
 
